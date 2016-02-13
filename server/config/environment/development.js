2b061545c67fb9405f86d238a8f822076f25ae69
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-02-13 18:29:08 +0700

    Add configuration files

---
+++ server/config/environment/development.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default {
+  mongo: {
+    uri: `mongodb://${process.env.DB_PORT_27017_TCP_ADDR || 'localhost'}/relay-dev`
+  }
+};
---
+++ server/config/environment/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import _ from 'lodash';
+
+let config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  graphql: {
+    port: 8000
+  }
+};
+
+export default _.extend(config, require(`./${config.env}`));
