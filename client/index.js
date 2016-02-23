66976d8bf0a56e20a65c754a587f4d9563e6a935
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-02-23 10:38:13 +0700

    Add client entry point and routers

---
+++ client/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RelayRouter } from 'react-router-relay';
+import { browserHistory} from 'react-router';
+
+import '../node_modules/react-mdl/extra/material.js';
+import Route from './routes/Route';
+
+const rootNode = document.createElement('div');
+document.body.appendChild(rootNode);
+
+ReactDOM.render(
+  <RelayRouter history={browserHistory} routes={Route} />,
+    rootNode
+);
---
+++ client/routes/ViewerQuery.js
@@ -0,0 +1,11 @@
+import Relay from 'react-relay';
+
+export default {
+  viewer: (Component) => Relay.QL`
+    query {
+      viewer {
+        ${Component.getFragment('viewer')}
+      }
+    }
+  `
+};
