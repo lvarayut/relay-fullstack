22243aebc54f871c9fdbc4a6496be43a872ecc01
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-02-23 10:54:09 +0700

    Add webpack configuration and package.json

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [
+      path.join(__dirname, 'client/index.js'),
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server'
+    ],
+    vendor: ['react']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  devtool: 'eval',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'postcss-loader']
+    }, {
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+      loader: 'url-loader?limit=10000'
+    }]
+  },
+  postcss: function() {
+    return [precss, autoprefixer];
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Relay Starter Kit - Integrated with Relay, GraphQL, Express, ES6/ES7, JSX, Webpack, Babel, Material Design Lite, and PostCSS',
+      template: './client/index.html',
+      mobile: true,
+      inject: false
+    })
+  ]
+};
+
