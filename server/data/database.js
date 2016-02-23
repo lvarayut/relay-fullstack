41ed24bed50aa9ed8583b5575bc11b6e05983453
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-02-23 10:53:06 +0700

    Add GraphQL schema

---
+++ server/data/database.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class User {
+  constructor(id, name, username, website) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.website = website;
+  }
+}
+
+class Feature {
+  constructor(id, name, description, url) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.url = url;
+  }
+}
+
+let lvarayut = new User('1', 'Varayut Lerdkanlayanawat', 'lvarayut', 'https://github.com/lvarayut/relay-fullstack');
+let features = [
+  new Feature('1', 'React', 'A JavaScript library for building user interfaces.', 'https://facebook.github.io/react'),
+  new Feature('2', 'Relay', 'A JavaScript framework for building data-driven react applications.', 'https://facebook.github.io/relay'),
+  new Feature('3', 'GraphQL', 'A reference implementation of GraphQL for JavaScript.', 'http://graphql.org'),
+  new Feature('4', 'Express', 'Fast, unopinionated, minimalist web framework for Node.js.', 'http://expressjs.com'),
+  new Feature('5', 'Webpack', 'Webpack is a module bundler that packs modules for the browser.', 'https://webpack.github.io'),
+  new Feature('6', 'Babel', 'Babel is a JavaScript compiler. Use next generation JavaScript, today.', 'https://babeljs.io'),
+  new Feature('7', 'PostCSS', 'PostCSS. A tool for transforming CSS with JavaScript.', 'http://postcss.org'),
+  new Feature('8', 'MDL', 'Material Design Lite lets you add a Material Design to your websites.', 'http://www.getmdl.io')
+];
+
+
+function getUser(id) {
+  return id === lvarayut.id ? lvarayut : null;
+}
+
+function getFeature(id) {
+  return features.find(w => w.id === id);
+}
+
+function getFeatures() {
+  return features;
+}
+
+export {
+  User,
+  Feature,
+  getUser,
+  getFeature,
+  getFeatures
+};
---
+++ server/data/schema.js
@@ -0,0 +1,149 @@
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString
+} from 'graphql';
+
+import {
+  connectionArgs,
+  connectionDefinitions,
+  connectionFromArray,
+  fromGlobalId,
+  globalIdField,
+  mutationWithClientMutationId,
+  nodeDefinitions
+} from 'graphql-relay';
+
+import {
+  User,
+  Feature,
+  getUser,
+  getFeature,
+  getFeatures
+} from './database';
+
+
+/**
+ * We get the node interface and field from the Relay library.
+ *
+ * The first method defines the way we resolve an ID to its object.
+ * The second defines the way we resolve an object to its GraphQL type.
+ */
+var {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    var {type, id} = fromGlobalId(globalId);
+    if (type === 'User') {
+      return getUser(id);
+    } else if (type === 'Feature') {
+      return getFeature(id);
+    } else {
+      return null;
+    }
+  },
+  (obj) => {
+    if (obj instanceof User) {
+      return userType;
+    } else if (obj instanceof Feature)  {
+      return featureType;
+    } else {
+      return null;
+    }
+  }
+);
+
+/**
+ * Define your own types here
+ */
+
+var userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'A person who uses our app',
+  fields: () => ({
+    id: globalIdField('User'),
+    features: {
+      type: featureConnection,
+      description: 'Features that I have',
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromArray(getFeatures(), args)
+    },
+    username: {
+      type: GraphQLString,
+      description: 'Users\'s username'
+    },
+    website: {
+      type: GraphQLString,
+      description: 'User\'s website'
+    }
+  }),
+  interfaces: [nodeInterface]
+});
+
+var featureType = new GraphQLObjectType({
+  name: 'Feature',
+  description: 'Feature integrated in our starter kit',
+  fields: () => ({
+    id: globalIdField('Feature'),
+    name: {
+      type: GraphQLString,
+      description: 'Name of the feature'
+    },
+    description: {
+      type: GraphQLString,
+      description: 'Description of the feature'
+    },
+    url: {
+      type: GraphQLString,
+      description: 'Url of the feature'
+    }
+  }),
+  interfaces: [nodeInterface]
+});
+
+/**
+ * Define your own connection types here
+ */
+var {connectionType: featureConnection} =
+  connectionDefinitions({name: 'Feature', nodeType: featureType});
+
+/**
+ * This is the type that will be the root of our query,
+ * and the entry point into our schema.
+ */
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    node: nodeField,
+    // Add your own root fields here
+    viewer: {
+      type: userType,
+      resolve: () => getUser('1')
+    }
+  })
+});
+
+/**
+ * This is the type that will be the root of our mutations,
+ * and the entry point into performing writes in our schema.
+ */
+var mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    // Add your own mutations here
+  })
+});
+
+/**
+ * Finally, we construct our schema (whose starting query type is the query
+ * type we defined above) and export it.
+ */
+export default new GraphQLSchema({
+  query: queryType
+  // Uncomment the following after adding some mutation fields:
+  // mutation: mutationType
+});
