ecb37e35d0d20df9c1d08a5664938d91c5c73599
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-02-23 10:53:35 +0700

    Add server entry point

---
+++ server/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import path from 'path';
+import webpack from 'webpack';
+import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import WebpackDevServer from 'webpack-dev-server';
+import gaze from 'gaze';
+import webpackConfig from '../webpack.config';
+import config from './config/environment';
+import schema from './data/schema';
+import updateSchema from './utils/updateSchema';
+
+(async() => {
+  try {
+    // Launch GraphQL
+    const graphql = express();
+    graphql.use('/', graphQLHTTP({
+      graphiql: true,
+      pretty: true,
+      schema: schema
+    }));
+    graphql.listen(config.graphql.port, () => console.log(`GraphQL is listening on port ${config.graphql.port}`));
+
+    // Launch Relay by using webpack.config.js
+    const app = new WebpackDevServer(webpack(webpackConfig), {
+      contentBase: '/build/',
+      proxy: {
+        '/graphql': `http://localhost:${config.graphql.port}`
+      },
+      stats: {
+        colors: true
+      },
+      hot: true,
+      historyApiFallback: true
+    });
+
+    // Serve static resources
+    app.use('/', express.static(path.join(__dirname, '../build')));
+    app.listen(config.port, () => {
+      console.log(`Relay is listening on port ${config.port}`);
+    });
+
+    // Update schema.json and schema.graphql
+    updateSchema();
+
+    // Watch JavaScript files in the data folder for changes, and update schema.json and schema.graphql
+    gaze(path.join(__dirname, 'data/*.js'), (err, watcher) => {
+      if (err) console.error('Error: Watching files in data folder');
+      watcher.on('all', () => {
+        updateSchema();
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
