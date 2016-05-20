97a3b50a5a07b3650a233ce1b12ea588d51f87b1
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-05-20 16:14:55 +0700

    Add a module name prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,19 +1,42 @@
 const yeoman = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const s = require('underscore.string');
 
 module.exports = yeoman.Base.extend({
   prompting() {
     this.log(yosay(
       `Welcome to ${chalk.yellow('Relay Fullstack Generator')}!`
     ));
+
+    return this.prompt([{
+      name: 'moduleName',
+      message: 'What would you like to name your app?',
+      default: 'relay-fullstack',
+      filter: n => s.slugify(n)
+    }]).then(answers => {
+      this.answers = {
+        moduleName: answers.moduleName,
+        appName: s(answers.moduleName).humanize().titleize().value()
+      };
+    });
   },
 
-  wrting() {
-    this.fs.copy(`${this.templatePath()}/relay-fullstack/{,.}**`, this.destinationPath());
+  writing() {
+    const tplPath = this.templatePath('relay-fullstack');
+    const desPath = this.destinationPath();
+
+    const copy = (from, to) => {
+      this.fs.copy(`${tplPath}/${from}`, `${desPath}/${to || from}`);
+    };
+
+    copy('client/index.html');
+    copy('client/assets/**', 'client/assets');
+    this.fs.copyTpl([`${tplPath}/{,.}**`, `!${tplPath}/client/index.html`, `!${tplPath}/client/assets/**`], desPath, this.answers);
   },
 
   install() {
     this.npmInstall();
   }
 });
+
--- test/app.js
+++ test/app.js
@@ -3,7 +3,7 @@ import path from 'path';
 import assert from 'yeoman-assert';
 import helpers from 'yeoman-test';
 
-test.beforeEach(() => helpers.run(path.join(__dirname, '../generators/app'), { skipInstall: true }).toPromise());
+test.beforeEach(() => helpers.run(path.join(__dirname, '../generators/app'), { skipInstall: true }).withPrompts({ moduleName: 'awesome-project' }).toPromise());
 
 test('includes all expected files', () => {
   assert.file([
@@ -19,5 +19,13 @@ test('includes all expected files', () => {
     'client',
     'server'
   ]);
+});
+
+test('binds appName and moduleName', () => {
+  assert.fileContent('package.json', /"name": "awesome-project"/);
+  assert.fileContent('package.json', /"description": "Awesome Project, Powered by Relay Fullstack"/);
+  assert.fileContent('webpack.config.js', /title: 'Awesome Project, Powered by Relay Fullstack'/);
+  assert.fileContent('webpack.production.config.js', /title: 'Awesome Project, Powered by Relay Fullstack'/);
+  assert.fileContent('client/components/Navbar/NavbarComponent.js', /const title = 'Awesome Project'/);
 });
 
