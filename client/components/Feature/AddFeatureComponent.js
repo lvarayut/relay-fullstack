ff982bfac7d159b3643ce60507a5464c81b563c5
Narongdej Sarnsuwan <zenyaith@gmail.com>
2016-08-01 13:07:13 +0700

    Add feature mutation (#28)
    * Add feature mutation
    
    * Change name to be consistency
    
    * Change folder structure of mutation

---
+++ client/components/Feature/AddFeatureComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Grid, Cell, Button } from 'react-mdl';
+import Page from '../Page/PageComponent';
+import Dropdown from 'react-dropdown';
+import AddFeatureMutation from './AddFeatureMutation';
+import Relay from 'react-relay';
+
+const options = [
+  { value: 'none', label: 'Please select a feature' },
+  { value: 'nodejs', label: 'Node.js' },
+  { value: 'html', label: 'HTML5' },
+  { value: 'css', label: 'CSS3' },
+];
+
+const inputData = {
+  nodejs: { name: 'Nodejs', url: 'https://nodejs.org', description: 'Node.js® is a JavaScript runtime built on Chrome\'s V8 JavaScript engine' },
+  html: { name: 'Html5', url: 'https://www.w3.org/TR/html5/', description: '5th major revision of the core language of the World Wide Web' },
+  css: { name: 'css', url: 'https://www.w3.org/Style/CSS/Overview.en.html', description: 'Cascading Style Sheets (CSS) is a simple mechanism for adding style to Web documents.' }
+};
+
+export default class Feature extends React.Component {
+  static propTypes = {
+    viewer: React.PropTypes.object.isRequired
+  };
+
+  state = {
+    form: {
+      dropdown: options[0]
+    }
+  }
+
+  onSelect = (e) => {
+    this.setState({ form: { dropdown: e } });
+  }
+
+  addFeature = () => {
+    const value = this.state.form.dropdown.value;
+    if (value === 'none') {
+      return;
+    }
+
+    const addFeatureMutation = new AddFeatureMutation({ viewerId: this.props.viewer.id, ...inputData[value] });
+    Relay.Store.commitUpdate(addFeatureMutation);
+  }
+
+  render() {
+    return (
+      <Page heading='Add a Feature'>
+        <Grid>
+          <Cell col={9}>
+            <Dropdown options={options} onChange={this.onSelect.bind(this)} value={this.state.form.dropdown} />
+          </Cell>
+          <Cell col={3} style={{ textAlign: 'center' }}>
+            <Button raised accent onClick={this.addFeature.bind(this)}>Add Feature</Button>
+          </Cell>
+        </Grid>
+      </Page>
+    );
+  }
+}
--- client/components/Feature/FeatureComponent.js
+++ client/components/Feature/FeatureComponent.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 import Page from '../Page/PageComponent';
 import styles from './Feature.scss';
+import AddFeature from './AddFeatureComponent';
 
 export default class Feature extends React.Component {
   static propTypes = {
@@ -11,26 +12,29 @@ export default class Feature extends React.Component {
 
   render() {
     return (
-      <Page heading='Integrated with'>
-        <Grid>
-          {this.props.viewer.features.edges.map(edge => {
-            const imageUrl = require(`../../assets/${edge.node.name.toLowerCase()}.png`);
-            return (
-              <Cell col={4} key={edge.node.id}>
-                <Card className={styles.card}>
-                  <CardTitle expand className={styles.image} style={{ backgroundImage: `url(${imageUrl})` }} />
-                  <CardActions className={styles.name}>
-                    <Button colored href={edge.node.url}>{edge.node.name}</Button>
-                  </CardActions>
-                  <CardText className={styles.description}>
-                    {edge.node.description}
-                  </CardText>
-                </Card>
-              </Cell>
-            );
-          })}
-        </Grid>
-      </Page>
+      <div>
+        <Page heading='Integrated with'>
+          <Grid>
+            {this.props.viewer.features.edges.map(edge => {
+              const imageUrl = require(`../../assets/${edge.node.name.toLowerCase()}.png`);
+              return (
+                <Cell col={4} key={edge.node.id}>
+                  <Card className={styles.card}>
+                    <CardTitle expand className={styles.image} style={{ backgroundImage: `url(${imageUrl})` }} />
+                    <CardActions className={styles.name}>
+                      <Button colored href={edge.node.url}>{edge.node.name}</Button>
+                    </CardActions>
+                    <CardText className={styles.description}>
+                      {edge.node.description}
+                    </CardText>
+                  </Card>
+                </Cell>
+              );
+            })}
+          </Grid>
+        </Page>
+        <AddFeature viewer={this.props.viewer} />
+      </div>
     );
   }
 }
