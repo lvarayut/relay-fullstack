9d92788c826a171731ac87b8ac405abc9e00339c
Nicholas Romero <ncrmro@gmail.com>
2017-02-19 11:27:32 -0600

    Adds example createToken mutation and helper functions

---
+++ server/utils/jwtToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+/* import { getUser } from '../data/database';*/
+
+const signJWT = ({ id }) => {
+  // Accepts an id and creates a signed token
+  const secret = process.env.JWT_SECRET || 'topsecret';
+  // sync https://github.com/auth0/node-jsonwebtoken/issues/111
+  return jwt.sign({ id }, secret, { expiresIn: '7d' });
+};
+
+const getJWT = (/* { username, password }*/) => {
+  /* const user = getUser(1);*/
+  const jwtToken = signJWT(1);
+  return { jwtToken };
+};
+
+export default getJWT;
