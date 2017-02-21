46ee0d9e5c05efbb073c8896d76712cba55b7def
Nigel Schuster <nigel.schusters@googlemail.com>
2017-02-21 10:50:30 -0500

    Added simple generator

---
+++ generators/config/index.js
@@ -0,0 +1,22 @@
+var Generator = require('yeoman-generator');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = class extends Generator {
+  _copyRec(src, dest) {
+    fs.readdir(src, (err, files) => {
+      files.forEach(file => {
+        fs.stat(path.join(src, file), (err, stats) => {
+          if(stats.isDirectory()) {
+            this._copyRec(path.join(src, file), path.join(dest, file));
+          } else {
+            this.fs.copy(path.join(src, file), path.join(dest, file));
+          }
+        });
+      })
+    });
+  }
+  writing() {
+    this._copyRec(this.templatePath('.'), this.destinationPath('.'))
+  }
+}
