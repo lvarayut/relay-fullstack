0f9906724711b63d453059e5936a2f679b2f4bee
Caio Flores <caiopflores@gmail.com>
2017-09-20 16:37:15 -0300

    Testing

--- client/routes/Route.js
+++ client/routes/Route.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { IndexRoute, Route, Redirect } from "react-router";
+import React from 'react';
+import { IndexRoute, Route, Redirect } from 'react-router';
 
-import ViewerQuery from "./ViewerQuery";
-import AppContainer from "../components/App/AppContainer";
-import FeatureContainer from "../components/Feature/FeatureContainer";
-import SignupComponent from "../components/Signup/SignupComponent";
-import LoginComponent from "../components/Login/LoginComponent";
+import ViewerQuery from './ViewerQuery';
+import AppContainer from '../components/App/AppContainer';
+import FeatureContainer from '../components/Feature/FeatureContainer';
+import SignupComponent from '../components/Signup/SignupComponent';
+import LoginComponent from '../components/Login/LoginComponent';
 
 export default (
   <Route path="/" component={AppContainer} queries={ViewerQuery}>
