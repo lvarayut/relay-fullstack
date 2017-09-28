66dbf6da223ec4a12ecaf608e02da8a8a4a0c288
Caio Flores <caioflores@macbook-caioflores.local>
2017-09-28 17:33:36 -0300

    Added jest for testing

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 let appEntry;
 let devtool;
